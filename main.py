22e1d7e33a2d15abc011d7ceb15751f39b4d3c39
Mikita Bulankou <bulankovby@gmail.com>
2025-02-13 20:30:16 +0300

    feat: complete HW#7

--- main.py
+++ main.py
@@ -3,84 +3,51 @@ import os
 os.system("clear")
 
 
-# Напишите программу, которая добавляет ‘ing’ к словам
-# вторая реализация будет ниже
-def gerund(word):
-    if word.endswith("ing"):
-        return word
+def hwTime(n):
+    if type(n) is int and n >= 0:
+        hours = (n // 60) % 24
+        rem_minutes = n % 60
+
+        time_str = f"{hours:02d}:{rem_minutes:02d}"
+
+        time_sum = sum(int(char) for char in time_str if char.isdigit())
+
+        print(f"Вы ввели: n = {n}, ответ : {time_sum}")
     else:
-        return word + "ing"
+        print("Проверь ввод!")
 
 
-words = ["sing", "clean", "concatinating", "jump"]
-ing_words = [gerund(word) for word in words]
+def hwLevelUp(exp, threshold, reward):
+    inpt = f"Вы ввели: exp = {exp}, threshold = {threshold}, reward = {reward}, "
 
-print(ing_words)
+    res = True
+
+    if type(exp) is int and type(threshold) is int and type(reward) is int:
+        total_exp = exp + reward
+        if total_exp >= threshold:
+            res = True
+            print(inpt, f"ответ: {res}")
+        else:
+            res = False
+            print(inpt, f"ответ: {res}")
+    else:
+        print("Проверь ввод!")
 
 
-def hw():
-    # Напишите программу, которая добавляет ‘ing’ к словам
-    line1 = "jump"
-    print(f"{line1} => {line1 + 'ing'}")
-
-    # Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
-    str_task1 = "www.my_site.com#about"
-    print(str_task1.replace("#", "/"))
-
-    # В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
-    str_task3 = "Ivanou Ivan"
-    print(" ".join(str_task3.split()[::-1]))
-
-    # Напишите программу которая удаляет пробел в начале, в конце строки
-    str_task3 = " Given text was with spaces on both ends "
-    print("'", str_task3.strip(), "'", sep="")  # Чтобы было видно стрип в консоли
-
-    # Имена собственные всегда начинаются с заглавной буквы,
-    # за которой следуют строчные буквы. Исправьте данное имя собственное так,
-    # чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
-
-    str_task4 = "pARiS"
-    print(str_task4.capitalize())
-
-    # "I love arrays they are my favorite"  =>
-    # => ["I", "love", "arrays", "they", "are", "my", "favorite"]
-    str_task5 = "I love arrays they are my favorite"
-    print(str_task5.split(" "))
-
-    # Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus напечатайте текст:
-    # “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
-    namesurname = ["Ivan", "Ivanou"]
-    city = "Minsk"
-    country = "Belarus"
-
-    print(
-        f"Привет, {namesurname[0]} {namesurname[1]}! Добро пожаловать в {city}, {country}"
-    )
-
-    # Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
-    #  сделайте из него строку
-    # => "I love arrays they are my favorite"
-
-    task6 = ["I", "love", "arrays", "they", "are", "my", "favorite"]
-    print(" ".join(task6))
-
-    # Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]
-    task6 = "Robin Singh"
-    print(task6.split())
-
-    # Создайте список из 10 элементов,
-    # вставьте на 3-ю позицию новое значение,
-    # удалите элемент из списка под индексом 6
-
-    task7 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-    new_value = 100
-    task7.insert(2, new_value)
-
-    del task7[6]
-
-    print(task7)
+def hwTimeConverter(time24):
+    if type(time24) is str and len(time24) == 5:
+        hours, minutes = int(time24.split(":")[0]), int(time24.split(":")[1])
+        am_pm = "a.m." if hours < 12 else "p.m."
+        hours = hours % 12
+        if hours == 0:
+            hours = 12
+        time12 = f"{hours}:{minutes:02d} {am_pm}"
+        print(f"Вы ввели - {time24}, ответ: {time12}")
+    else:
+        print("Проверь ввод!")
 
 
 if __name__ == "__main__":
-    hw()
+    hwTime(808)
+    hwLevelUp(10, 15, 5)
+    hwTimeConverter("23:12")
