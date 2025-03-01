ab23dfe9e06f8a6b4d9ab42266ca3498e908a0b8
Mikita Bulankou <bulankovby@gmail.com>
2025-03-01 17:01:30 +0300

    init hw12

---
+++ bank.py
@@ -0,0 +1,12 @@
+class Bank:
+    def register_client(self):
+        pass
+
+    def open_deposit_account(self):
+        pass
+
+    def calc_deposit_interest_rate(self):
+        pass
+
+    def close_deposit(self):
+        pass
---
+++ library.py
@@ -0,0 +1,26 @@
+class Book:
+    def reserver(self):
+        pass
+
+    def cancel_reserve(self):
+        pass
+
+    def get_book(self):
+        pass
+
+    def return_book(self):
+        pass
+
+
+class Reader:
+    def reserve_book(self):
+        pass
+
+    def cancel_reserve(self):
+        pass
+
+    def get_book(self):
+        pass
+
+    def return_book(self):
+        pass
