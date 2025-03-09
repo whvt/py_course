f01688c1e40d10e4f8ad3d59ae76c299dca5a88a
Mikita Bulankou <bulankovby@gmail.com>
2025-03-09 21:07:58 +0300

    refactor: bank task

--- bank.py
+++ bank.py
@@ -12,63 +12,84 @@ class Deposit:
             self.current_balance += self.current_balance * monthly_rate
         return round(self.current_balance, 2)
 
+    def get_summary(self):
+        return {
+            "Start Balance": self.start_balance,
+            "Current Balance": self.current_balance,
+            "Years": self.years,
+            "Interest Rate": self.interest_rate,
+        }
+
 
 class Client:
     def __init__(self, client_id, name):
         self.client_id = client_id
         self.name = name
-        self.deposit_account = None
 
-    def open_deposit_account(self, start_balance, years):
-        self.deposit_account = Deposit(start_balance, years)
-
-    def calc_interest_rate(self):
-        if self.deposit_account:
-            return self.deposit_account.calculate_monthly_compound_interest()
-        else:
-            raise ValueError("No deposit account found.")
-
-    def close_deposit(self):
-        if self.deposit_account:
-            final_amount = self.calc_interest_rate()
-            self.deposit_account = None
-            return final_amount
-        else:
-            raise ValueError("No deposit account found.")
+    def __str__(self):
+        return f"Client ID: {self.client_id}, Name: {self.name}"
 
 
 class Bank:
     def __init__(self):
         self.clients = {}
+        self.deposits = {}
 
     def register_client(self, client_id, name):
         if client_id not in self.clients:
             self.clients[client_id] = Client(client_id, name)
+        else:
+            print(f"Client {client_id} is already registered.")
 
     def open_deposit_account(self, client_id, start_balance, years):
         if client_id in self.clients:
-            self.clients[client_id].open_deposit_account(start_balance, years)
+            if client_id not in self.deposits:
+                self.deposits[client_id] = Deposit(start_balance, years)
+                print(f"Deposit account opened for Client {client_id}.")
+            else:
+                print(f"Client {client_id} already has a deposit account.")
+        else:
+            print(f"Client {client_id} not found. Register the client first.")
 
     def calc_interest_rate(self, client_id):
-        if client_id in self.clients:
-            return self.clients[client_id].calc_interest_rate()
+        if client_id in self.deposits:
+            return self.deposits[client_id].calculate_monthly_compound_interest()
         else:
-            raise ValueError("Client not found.")
+            raise ValueError("No deposit account found.")
 
     def close_deposit(self, client_id):
-        if client_id in self.clients:
-            return self.clients[client_id].close_deposit()
+        if client_id in self.deposits:
+            final_amount = self.deposits[
+                client_id
+            ].calculate_monthly_compound_interest()
+            del self.deposits[client_id]
+            print(
+                f"Deposit account for Client {client_id} closed. Final amount: {final_amount}"
+            )
+            return final_amount
         else:
-            raise ValueError("Client not found.")
+            raise ValueError("No deposit account found.")
 
+    def show_client_info(self, client_id):
+        if client_id in self.clients:
+            print(self.clients[client_id])
+            if client_id in self.deposits:
+                print(self.deposits[client_id].get_summary())
+            else:
+                print("No deposit account found.")
+        else:
+            print(f"Client {client_id} not found.")
 
-# client_id = "0000001"
 
 bank = Bank()
+
 bank.register_client(client_id="0000001", name="Nick")
-bank.open_deposit_account(client_id="0000001", start_balance=1000, years=1)
 
-assert bank.calc_interest_rate(client_id="0000001") == 1104.71, "<Error message>"
+bank.open_deposit_account("0000001", 1000, 1)
 
-final_balance = bank.close_deposit(client_id="0000001")
-print(f"Final balance after closing the deposit: {final_balance}")
+
+print("Final balance after interest:", bank.calc_interest_rate("0000001"))
+
+bank.show_client_info("0000001")
+
+bank.close_deposit("0000001")
