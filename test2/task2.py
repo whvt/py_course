c49b783816f7314a37400a988ca2d77295deaf58
Mikita Bulankou <bulankovby@gmail.com>
2025-03-18 19:33:50 +0300

    feat: complete task2

---
+++ test2/task2.py
@@ -0,0 +1,14 @@
+class ProcessNum:
+    def __init__(self, input_num):
+        self.input_num = input_num
+
+    def get_square(self):
+        input_num_valid = isinstance(self.input_num,int)
+        return self.input_num * self.input_num if input_num_valid else None
+    def is_odd(self):
+        return True if (self.input_num % 2 == 0 and self.input_num != 0) else False
+
+reference = ProcessNum(12)
+
+print('References square is:', reference.get_square())
+print('Given number is odd? ->', reference.is_odd())
