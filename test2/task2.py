9d7cb1ee16931adc8ee0bf1d2434485b07b02e66
Mikita Bulankou <bulankovby@gmail.com>
2025-03-18 20:28:20 +0300

    ruff:format

--- test2/task2.py
+++ test2/task2.py
@@ -3,12 +3,14 @@ class ProcessNum:
         self.input_num = input_num
 
     def get_square(self):
-        input_num_valid = isinstance(self.input_num,int)
+        input_num_valid = isinstance(self.input_num, int)
         return self.input_num * self.input_num if input_num_valid else None
+
     def is_odd(self):
         return True if (self.input_num % 2 == 0 and self.input_num != 0) else False
 
+
 reference = ProcessNum(12)
 
-print('References square is:', reference.get_square())
-print('Given number is odd? ->', reference.is_odd())
+print("References square is:", reference.get_square())
+print("Given number is odd? ->", reference.is_odd())
--- test2/task3.py
+++ test2/task3.py
@@ -1,8 +1,8 @@
 def calculate_sum_upto(n):
     pos = sum(range(1, n + 1))
-    neg = sum(range(n,1))
+    neg = sum(range(n, 1))
 
-    return pos if n>0 else neg
+    return pos if n > 0 else neg
 
 
 number = -2
